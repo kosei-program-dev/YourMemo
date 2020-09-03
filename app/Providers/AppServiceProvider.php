b62778dae9d9788427e65cf2239a6e7db9e8a6d8
kosei-program-dev <kosei.program.dev@gmail.com>
2020-09-04 00:57:08 +0900

    Add Procfile + varchar = 191

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Schema;    //追記
 use Faker\Generator as FakerGenerator;    // 追記
 use Faker\Factory as FakerFactory;      // 追記
 

440d4a2454a39fa72722365b68db75742329ade4
kosei-program-dev <kosei.program.dev@gmail.com>
2020-09-03 23:46:47 +0900

    カラムの最大長を変更し、767bytes以上の文字列が入らないようにする

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 use Faker\Generator as FakerGenerator;    // 追記
 use Faker\Factory as FakerFactory;      // 追記
 
@@ -28,6 +29,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 }
